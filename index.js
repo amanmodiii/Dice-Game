ecf26d4ffcf3ad4941d515d31c8d13936b93af87
amanmodiii <97941987+amanmodiii@users.noreply.github.com>
2022-06-18 12:57:25 +0530

    /images

---
+++ index.js
@@ -0,0 +1,27 @@
+var name1 = "Player1";
+var name2 = "Player2";
+
+name1 = prompt("Enter player 1's name : ");
+document.querySelector(".pl1").innerHTML=name1;
+
+name2 = prompt("Enter player2's name : ");
+document.querySelector(".pl2").innerHTML=name2;
+
+document.querySelector(".btn").addEventListener("click",function (){
+
+  var randomNumber1=Math.floor(Math.random()*5+1);
+  document.querySelector(".image1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+  var randomNumber2=Math.floor(Math.random()*5+1);
+  document.querySelector(".image2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent=name1 + " wins."
+  }
+  else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent=name2 + " wins."
+  }
+  else{
+    document.querySelector("h1").textContent="It's a Draw."
+  }
+});
